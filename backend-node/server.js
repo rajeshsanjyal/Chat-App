c93ec0e3bc8df8820699d004c345afaf9aef57cd
rajeshsanjyal <rajesh.sanjyal@gmail.com>
2024-09-26 00:11:15 +0545

    Connected to database

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,7 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import mongoose from "mongoose"
 dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected to Mongodb");        
+    })
+    .catch((err)=>{
+        console.log(err);        
+    });
 
 const app = express()
 const PORT = process.env.PORT || 3000
