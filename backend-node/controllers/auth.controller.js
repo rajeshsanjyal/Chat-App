09d549aaef9c7ae368865d9b2e46a6890dbfce33
rajeshsanjyal <rajesh.sanjyal@gmail.com>
2024-09-26 09:16:28 +0545

    Login route

--- backend-node/controllers/auth.controller.js
+++ backend-node/controllers/auth.controller.js
@@ -47,9 +47,28 @@ export const signup = async (req, res,next) => {
     }
 };
 
-export const login = (req, res) => {
-    // Login logic here
-    res.send("User logged in");
+export const login = async (req, res, next) => {
+    try {
+       const {email,password} = req.body
+       const validUser = await User.findOne({email: email})
+       if(!validUser){
+        return next(errorHandler(404),"User not found")
+       }
+
+       const validPassword = bcryptjs.compareSync(password, validUser.password)
+       if(!validPassword){
+        return next(errorHandler(401, "Wrong Credentials!"));
+       }
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET)
+        res.cookie("access_token", token, {httpOnly: true}).status(200).json({
+            _id: validUser._id,
+            username: validUser.username,
+            email: validUser.email,
+            profilePic: validUser.profilePic
+        })
+    } catch (error) {
+        next(error)
+    }
 };
 
 export const logout = (req, res) => {
