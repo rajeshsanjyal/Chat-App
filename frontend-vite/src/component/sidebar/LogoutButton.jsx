90aeb51a9a312d9002deabbf2743c054ad6e75fe
rajeshsanjyal <rajesh.sanjyal@gmail.com>
2024-09-27 08:49:09 +0545

    search button added

---
+++ frontend-vite/src/component/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogoutButton = () => {
+  return (
+    <div>
+      Logout Button
+    </div>
+  )
+}
+
+export default LogoutButton
--- frontend-vite/src/pages/Home.jsx
+++ frontend-vite/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from '../component/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200'>
+      <Sidebar />
     </div>
   )
 }
