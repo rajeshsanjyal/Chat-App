050ac475268807f57756bf541c0ceb7f2310ccc4
rajeshsanjyal <rajesh.sanjyal@gmail.com>
2024-10-12 07:05:18 +0545

    Logout functionality

--- frontend-vite/src/pages/SignUp.jsx
+++ frontend-vite/src/pages/SignUp.jsx
@@ -99,8 +99,15 @@ const SignUp = () => {
             Already have an account?
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 bg-blue-700 text-white">
-              Signup
+            <button
+              className="btn btn-block btn-sm mt-2 bg-blue-700 text-white"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
