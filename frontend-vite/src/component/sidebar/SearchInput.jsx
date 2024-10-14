5d183288291bd752c4127ba40c22bd416e8a7e59
rajeshsanjyal <rajesh.sanjyal@gmail.com>
2024-10-14 12:57:12 +0545

    search input functionality

--- frontend-vite/src/component/sidebar/SearchInput.jsx
+++ frontend-vite/src/component/sidebar/SearchInput.jsx
@@ -1,19 +1,42 @@
-import React from "react";
-import { IoSearchSharp } from "react-icons/io5"
+import React, { useState } from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversation from "../../hooks/useGetConversation";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("")
+  const { setSelectedConversation } = useConversation()
+  const { conversations } = useGetConversation()
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    if(!search){
+      return
+    }
+
+    const conversation = conversations.find((con)=>
+      con.username.toLowerCase().includes(search.toLowerCase())
+    )
+    if(conversation){
+      setSelectedConversation(conversation)
+      setSearch("")
+    }else{
+      toast.error("No user found with the given keyword")
+    }
+
+  }
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <button
-        type="submit"
-        className="btn btn-circle bg-sky-500 text-white"
-      >
-        <IoSearchSharp className="w-6 h-6 outline-none"/>
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <IoSearchSharp className="w-6 h-6 outline-none" />
       </button>
     </form>
   );
